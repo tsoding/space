e23af57a94b4394e54a8df75f982c0837bdf9b49
rexim <reximkut@gmail.com>
2016-10-30 01:51:21 +0600

    Regenerate star according to the direction

--- index.js
+++ index.js
@@ -1,3 +1,68 @@
+function is_pos(x) {
+    return x > 0;
+}
+
+function is_neg(x) {
+    return x < 0;
+}
+
+function is_zero(x) {
+    return x === 0;
+}
+
+function snap_with_direction(point, direction, width, height) {
+    const snappers =
+              [or(snap_axis(0, 0), snap_axis(1, 0)),
+               or(snap_axis(0, width), snap_axis(1, 0)),
+               or(snap_axis(0, width), snap_axis(1, height)),
+               or(snap_axis(0, 0), snap_axis(1, height)),
+
+               snap_axis(0, 0),
+               snap_axis(1, 0),
+               snap_axis(0, width),
+               snap_axis(1, height),
+
+               nothing()];
+    snappers[kind_of_vector(direction)](point);
+}
+
+function kind_of_vector(vector) {
+    return [[is_pos, is_pos],   // quadrants
+            [is_neg, is_pos],
+            [is_neg, is_neg],
+            [is_pos, is_neg],
+
+            [is_pos, is_zero],  // axis
+            [is_zero, is_pos],
+            [is_neg, is_zero],
+            [is_zero, is_neg],
+
+            [is_zero, is_zero]] // special case
+        .findIndex(function(predicate) {
+            return predicate[0](vector[0]) && predicate[1](vector[1]);
+        });
+}
+
+function snap_axis(axis, value) {
+    return function(point) {
+        point[axis] = value;
+    };
+}
+
+function or(f1, f2) {
+    return function(x) {
+        if (Math.random() * 2 < 1) {
+            f1(x);
+        } else {
+            f2(x);
+        }
+    };
+}
+
+function nothing() {
+    return function(x) {};
+}
+
 function Ship(position, width, height) {
 
     this.position = position;
@@ -11,7 +76,7 @@ function Ship(position, width, height) {
 
 function StarsEffect(starsCount, width, height) {
     const STAR_SPEED = 300.0;
-    const DIRECTION = [-1, 0];
+    const DIRECTION = [-0.50, -1.0];
 
     this.stars = [];
 
@@ -25,10 +90,11 @@ function StarsEffect(starsCount, width, height) {
             star[1] += DIRECTION[1] * STAR_SPEED * star[2] * (deltaTime * 0.001);
 
             if (star[0] < 0 || star[0] >= width || star[1] < 0 || star[1] >= height) {
-                // TODO: regenerate star according to the DIRECTION
-                star[0] = width;
+                star[0] = Math.random() * width;
                 star[1] = Math.random() * height;
                 star[2] = Math.random();
+
+                snap_with_direction(star, DIRECTION, width, height);
             }
         });
     };
